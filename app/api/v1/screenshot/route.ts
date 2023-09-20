c4f4d107c62099024910f695816e1d2356cfb6ea
Michal Pasierbski <mpasierbski@gmail.com>
2023-09-21 00:33:43 +0200

    Add lambda auth

--- app/api/v1/screenshot/route.ts
+++ app/api/v1/screenshot/route.ts
@@ -2,11 +2,10 @@ import { NextResponse } from "next/server";
 import { sizePreset } from "@/lib/sizePresets";
 
 const CDN_BASE_URL = "https://cdn.siteshooter.app/";
+const { LAMBDA_ACCESS_KEY } = process.env;
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  console.log(searchParams);
-
   const urlRaw = searchParams.get("url");
   const preset = sizePreset(searchParams.get("preset"));
 
@@ -14,24 +13,39 @@ export async function GET(request: Request) {
     return NextResponse.error();
   }
 
-  const screenshotUrl = await fetch(
-    " https://2qcip9gtv1.execute-api.eu-west-1.amazonaws.com/Prod/",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        url: new URL(urlRaw).toString(),
-        width: preset[0],
-        height: preset[1],
-        removeEl: searchParams.getAll("removeEl"),
-      }),
-    }
-  ).then((res) => res.text());
+  let errorResponse = null;
+  try {
+    const screenshotUrl = await fetch(
+      " https://2qcip9gtv1.execute-api.eu-west-1.amazonaws.com/Prod/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${LAMBDA_ACCESS_KEY}`,
+        },
+        body: JSON.stringify({
+          url: new URL(urlRaw).toString(),
+          width: preset[0],
+          height: preset[1],
+          removeEl: searchParams.getAll("removeEl"),
+        }),
+      }
+    )
+      .then((res) => {
+        if (!res.ok) {
+          errorResponse = res;
+          throw new Error("Failed to generate screenshot");
+        }
 
-  const url = new URL(screenshotUrl);
-  const cdnUrl = new URL(url.pathname, CDN_BASE_URL);
+        return res;
+      })
+      .then((res) => res.text());
 
-  return Response.redirect(cdnUrl, 301);
+    const url = new URL(screenshotUrl);
+    const cdnUrl = new URL(url.pathname, CDN_BASE_URL);
+
+    return Response.redirect(cdnUrl, 301);
+  } catch (e) {
+    return errorResponse || NextResponse.error();
+  }
 }
