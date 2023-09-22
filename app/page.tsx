9034c17f2eba33a32c20f6c80ed858a4b41cc127
Michal Pasierbski <mpasierbski@gmail.com>
2023-09-22 13:28:17 +0200

    Add tally form

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,41 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { LaunchTally } from "@/src/launch-tally";
 
 export default function Home() {
   return (
-    <div className="container">
-      <header>
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl my-4">
-          Screenshot as a Service.
-          <br />
-          No libraries, just the URL.
-        </h1>
+      <div className="container">
+        <header>
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl my-4">
+            Screenshot as a Service.
+            <br />
+            No libraries, just the URL.
+          </h1>
 
-        <p>
-          As simple as
-          <a
-            href="https://www.siteshooter.app/api/shot?url=https://www.siteshooter.app"
-            target="_blank">
-            <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-              https://www.siteshooter.app/api/shot?url=https://www.siteshooter.app
-            </code>
-          </a>
-        </p>
+          <p>
+            As simple as
+            <a
+              href="https://www.siteshooter.app/api/shot?url=https://www.siteshooter.app"
+              target="_blank"
+            >
+              <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+                https://www.siteshooter.app/api/shot?url=https://www.siteshooter.app
+              </code>
+            </a>
+          </p>
 
-        <Button asChild className="my-4">
-          <Link href="/playground">Try it out!</Link>
-        </Button>
+          <Button asChild className="my-4">
+            <Link href="/playground">Try it out!</Link>
+          </Button>
 
-        <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
-          <li>Perfect for social media share 📱 preview images 🖼️.</li>
-        </ul>
-      </header>
+          <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+            <li>Perfect for social media share 📱 preview images 🖼️.</li>
+          </ul>
+        </header>
 
-      <section className="my-20"></section>
-    </div>
+        <section className="my-20">
+          <LaunchTally />
+        </section>
+      </div>
   );
 }
---
+++ src/launch-tally.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Script from "next/script";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+
+export function LaunchTally() {
+  return (
+    <>
+      <Card>
+        <CardHeader>
+            <h2 className="text-2xl font-bold">Siteshooter is going live!</h2>
+        </CardHeader>
+        <CardContent>
+          <iframe
+            data-tally-src="https://tally.so/embed/n0BXXj?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+            loading="lazy"
+            width="100%"
+            height="387"
+            frameBorder="0"
+            marginHeight={0}
+            marginWidth={0}
+            title="Coming soon!"
+          ></iframe>
+        </CardContent>
+      </Card>
+      <Script
+        id="tally-js"
+        src="https://tally.so/widgets/embed.js"
+        onLoad={() => {
+          window.Tally.loadEmbeds();
+        }}
+      />
+    </>
+  );
+}
