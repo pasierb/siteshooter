8c25528a41ccdf18c5a9d6b624552d9e3d06a301
Michal Pasierbski <mpasierbski@gmail.com>
2023-09-23 21:30:40 +0200

    Fix env variables in build

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,11 +1,12 @@
 import { Database } from "@/types/supabase";
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = () => createClient<Database>(
+export const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_API_KEY!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_API_KEY!
 );
-export const supabaseServiceRole = () => createClient<Database>(
+export const supabaseServiceRole = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_API_KEY!,
+  // This is a service role API key not available to the client code, only servser side.
+  process.env.SUPABASE_SERVICE_ROLE_API_KEY || "intentionally-invalid-key"
 );
